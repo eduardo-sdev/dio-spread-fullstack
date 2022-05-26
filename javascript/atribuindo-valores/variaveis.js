36f08b8de0fa4292889178ce2803458976d101af
eduardo-developer01 <eduardosilvaprogramador@gmail.com>
2022-05-26 10:18:24 -0300

    variaveis no javascript

---
+++ javascript/atribuindo-valores/variaveis.js
@@ -0,0 +1,17 @@
+let nbr
+nbr = 1
+console.log(nbr+6);
+
+var fistname = 'eduardo'
+var lastname = 'silva'
+
+if(fistname === 'eduardo') {
+    var fistname = 'eduardo'
+    let lastname = 'silva'
+    lastname = 'lima'
+
+    console.log(lastname);
+}
+
+console.log(fistname, lastname);
+
